b5213b7cdc6cb31e29cf256398b5e02d2de7dc33
hailegnaw <ixvos406@gmail.com>
2023-06-15 14:51:40 -0700

    4-puts.c updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,11 @@
 /*
  * This program prints the string "Programming is like building a multilingual puzzle"
  * followed by a new line using the puts function.
+ *
+ * Return: 0 on success
  */
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle\n");
-    return (0);
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
