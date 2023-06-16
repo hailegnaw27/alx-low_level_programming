dedd0f2d4b7a1199a82484e103a1a6bbd02e2364
hailegnaw <ixvos406@gmail.com>
2023-06-16 12:03:14 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,15 +6,17 @@
 int main(void)
 {
 int i;
-  char c;
-  for (i = 0; i < 10; i++) {
-    c = i + '0';
-    putchar(c);
-  }
-  for (i = 0; i < 6; i++) {
-    c = i + 'a';
-    putchar(c);
-  }
-  putchar('\n');
-  return 0;
+char c;
+for (i = 0; i < 10; i++) 
+{
+c = i + '0';
+putchar(c);
+}
+for (i = 0; i < 6; i++) 
+{
+c = i + 'a';
+putchar(c);
+}
+putchar('\n');
+return (0);
 }
