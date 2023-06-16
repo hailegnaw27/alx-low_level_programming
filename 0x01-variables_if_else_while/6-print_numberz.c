7718dddce8895fc3ca635adefc6145c3ed38f7e4
hailegnaw <ixvos406@gmail.com>
2023-06-16 11:56:02 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,14 +3,14 @@
 *main-Entry point.
 *Return: Always 0 (Success)
 */
-int main(void) 
+int main(void)
 {
 int num = 0;
-while (num < 10) 
+while (num < 10)
 {
 putchar(num + '0');
 num++;
-}    
+}
 putchar('\n');
 return 0;
 }
