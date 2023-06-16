3681843ac8ada66fca48cfd366a286390f143bbb
hailegnaw <ixvos406@gmail.com>
2023-06-16 12:12:13 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,21 +4,25 @@
 *Return: Always 0 (Success)
 */
 int main(void)
-{int i, j;
-  i = 0;
-  while (i < 10) {
-    j = i + 1;
-    while (j < 10) {
-      putchar(i + '0');
-      putchar(j + '0');
-      if (i != 8 || j != 9) {
-        putchar(',');
-        putchar(' ');
-      }
-      j++;
-    }
-    i++;
-  }
-  putchar('\n');
-  return 0;
+{
+int i, j;
+i = 0;
+while (i < 10)
+{
+j = i + 1;
+while (j < 10)
+{
+putchar(i + '0');
+putchar(j + '0');
+if (i != 8 || j != 9)
+{
+putchar(',');
+putchar(' ');
+}
+j++;
+}
+i++;
+}
+putchar('\n');
+return (0);
 }
