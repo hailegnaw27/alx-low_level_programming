0dc9bc9497f30bf0609bf45d6808d697f9f409ea
hailegnaw <ixvos406@gmail.com>
2023-06-16 12:16:36 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,27 +5,26 @@
 */
 int main(void)
 {
-	int main(void) {
-  int i, j, k;
-  i = 0;
-  while (i < 8) {
-    j = i + 1;
-    while (j < 9) {
-      k = j + 1;
-      while (k < 10) {
-        putchar(i + '0');
-        putchar(j + '0');
-        putchar(k + '0');
-        if (i != 7 || j != 8 || k != 9) {
-          putchar(',');
-          putchar(' ');
+    int i, j, k;
+
+    for (i = 0; i <= 7; i++)
+    {
+        for (j = i + 1; j <= 8; j++)
+        {
+            for (k = j + 1; k <= 9; k++)
+            {
+                putchar(i + '0');
+                putchar(j + '0');
+                putchar(k + '0');
+
+                if (i < 7 || j < 8 || k < 9)
+                {
+                    putchar(',');
+                    putchar(' ');
+                }
+            }
         }
-        k++;
-      }
-      j++;
     }
-    i++;
-  }
-  putchar('\n');
-  return 0;
+
+    return (0);
 }
