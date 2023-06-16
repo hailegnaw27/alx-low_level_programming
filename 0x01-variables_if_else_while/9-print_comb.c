0a7f1014df15a13c029231f08de28e29f17be191
hailegnaw <ixvos406@gmail.com>
2023-06-16 12:07:13 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,14 +6,16 @@
 int main(void)
 {
 int i = 0;
-  while (i < 10) {
-    putchar(i + '0');
-    if (i != 9) {
-      putchar(',');
-      putchar(' ');
-    }
-    i++;
-  }
-  putchar('\n');
-  return 0;
+while (i < 10)
+{
+putchar(i + '0');
+if (i != 9)
+{
+putchar(',');
+putchar(' ');
+}
+i++;
+}
+putchar('\n');
+return (0);
 }
