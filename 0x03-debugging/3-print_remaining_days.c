0da5397d81cab42b78fc5c0e03320e9a37f94f0a
hailegnaw <ixvos406@gmail.com>
2023-06-22 13:29:47 -0700

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -2,82 +2,82 @@
 #include "main.h"
 
 /**
-* convert_day - converts day of month to day of year, without accounting
-* for leap year
-* @month: month in number format
-* @day: day of month
-* @year: year
-* Return: day of year
-*/
+ * convert_day - converts day of month to day of year, without accounting
+ * for leap year
+ * @month: month in number format
+ * @day: day of month
+ * Return: day of year
+ */
 int convert_day(int month, int day)
 {
-switch (month)
-{
-case 2:
-day = 31 + day;
-break;
-case 3:
-day = 59 + day;
-break;
-case 4:
-day = 90 + day;
-break;
-case 5:
-day = 120 + day;
-break;
-case 6:
-day = 151 + day;
-break;
-case 7:
-day = 181 + day;
-break;
-case 8:
-day = 212 + day;
-break;
-case 9:
-day = 243 + day;
-break;
-case 10:
-day = 273 + day;
-break;
-case 11:
-day = 304 + day;
-break;
-case 12:
-day = 334 + day;
-break;
-default:
-break;
-}
-return (day);
+  switch (month)
+    {
+    case 2:
+      day = 31 + day;
+      break;
+    case 3:
+      day = 59 + day;
+      break;
+    case 4:
+      day = 90 + day;
+      break;
+    case 5:
+      day = 120 + day;
+      break;
+    case 6:
+      day = 151 + day;
+      break;
+    case 7:
+      day = 181 + day;
+      break;
+    case 8:
+      day = 212 + day;
+      break;
+    case 9:
+      day = 243 + day;
+      break;
+    case 10:
+      day = 273 + day;
+      break;
+    case 11:
+      day = 304 + day;
+      break;
+    case 12:
+      day = 334 + day;
+      break;
+    default:
+      break;
+    }
+  return (day);
 }
 
 /**
-* print_remaining_days - takes a date and prints how many days are
-* left in the year, taking leap years into account
-* @month: month in number format
-* @day: day of month
-* @year: year
-* Return: void
-*/
+ * print_remaining_days - takes a date and prints how many days are
+ * left in the year, taking leap years into account
+ * @month: month in number format
+ * @day: day of month
+ * @year: year
+ * Return: void
+ */
 void print_remaining_days(int month, int day, int year)
 {
-int days;
+  int days;
 
-if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
-{
-if (month > 2)
-{
-day++;
+  if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+    {
+      if (month > 2)
+	{
+	  day++;
+	}
+
+      days = 366 - (convert_day(month, day) + 1);
+    }
+  else
+    {
+      days = 365 - (convert_day(month, day) + 1);
+    }
+
+  printf("Day of the year: %d\n", convert_day(month, day));
+  printf("Remaining days: %d\n", days);
 }
 
-days = 366 - (convert_day(month, day) + 1);
-}
-else
-{
-days = 365 - (convert_day(month, day) + 1);
-}
-
-printf("Day of the year: %d\n", convert_day(month, day));
-printf("Remaining days: %d\n", days);
-}
