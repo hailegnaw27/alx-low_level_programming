5dd900100be937bd2ef200eb20b70136e43301bf
hailegnaw <ixvos406@gmail.com>
2023-06-23 11:59:23 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,43 +1,42 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Description: Prints numbers from 1 to 100 replacing
- * multiples of three with
- * the word "Fizz", multiples of five with "Buzz" and
- * multiples of both with
- * "FizzBuzz"
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Description: Prints numbers from 1 to 100 replacing
+* multiples of three with
+* the word "Fizz", multiples of five with "Buzz" and
+* multiples of both with
+* "FizzBuzz"
+*
+* Return: Always 0 (Success)
+*/
 
 int main(void)
 {
-    int i;
+int i;
 
-    for (i = 1; i <= 100; i++)
-    {
-        if (i % 3 == 0 && i % 5 == 0)
-        {
-            printf("FizzBuzz ");
-        }
-        else if (i % 3 == 0)
-        {
-            printf("Fizz ");
-        }
-        else if (i % 5 == 0)
-        {
-            printf("Buzz ");
-        }
-        else
-        {
-            printf("%d ", i);
-        }
-    }
-
-    printf("\n");
-
-    return (0);
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz ");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz ");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz ");
+}
+else
+{
+printf("%d ", i);
+}
 }
 
+printf("\n");
+
+return (0);
+}
