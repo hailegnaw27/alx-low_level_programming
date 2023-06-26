55772390d9c13ffea1e5a06ac4741aca1cc87fc2
hailegnaw <ixvos406@gmail.com>
2023-06-26 15:48:51 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <unistd.h>
 
 /**
  * _puts - prints a string to stdout
@@ -6,7 +7,6 @@
  *
  * Return: void
  */
-int _putchar(char c);
 void _puts(char *str)
 {
     int i = 0;
@@ -17,4 +17,16 @@ void _puts(char *str)
         i++;
     }
     _putchar('\n');
+}
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+    return (write(STDOUT_FILENO, &c, 1));
 }
