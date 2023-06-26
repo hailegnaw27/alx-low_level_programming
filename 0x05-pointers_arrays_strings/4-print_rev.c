69dd3509d2750017f2b59c5b851bf0e557c93c4d
hailegnaw <ixvos406@gmail.com>
2023-06-26 13:08:49 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse to stdout
+ * @s: Pointer to the string to print in reverse
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+    int i = 0;
+
+    while (s[i] != '\0')
+    {
+        i++;
+    }
+
+    while (i > 0)
+    {
+        _putchar(s[i - 1]);
+        i--;
+    }
+    _putchar('\n');
+}
