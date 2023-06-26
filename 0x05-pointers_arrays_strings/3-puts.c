cef326193e3d0a3c87d1165ad5054feaa048dfc2
hailegnaw <ixvos406@gmail.com>
2023-06-26 15:46:11 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -6,6 +6,7 @@
  *
  * Return: void
  */
+int _putchar(char c);
 void _puts(char *str)
 {
     int i = 0;
@@ -17,16 +18,3 @@ void _puts(char *str)
     }
     _putchar('\n');
 }
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-    return (write(1, &c, 1));
-}
-
