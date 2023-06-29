c4f1d96a954fa27b1bb3d819f2f430ad77375ffd
hailegnaw <ixvos406@gmail.com>
2023-06-29 12:48:34 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,4 +1,4 @@
-**
+/**
 * _strcmp - compares two strings
 * @s1: first string
 * @s2: second string
@@ -13,6 +13,6 @@ int i = 0;
 while (s1[i] == s2[i])
 {
 if (s1[i] == '\0')
-return 0;
+return (0);
 i++;
 }
