c0ad24b8efb7e9b55d6077ae6efa593ff5c36a97
hailegnaw <ixvos406@gmail.com>
2023-06-29 12:58:08 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,21 +1,20 @@
 /**
- * _strcmp - Compares two strings
- * @s1: First string
- * @s2: Second string
- *
- * Return: Difference between the ASCII values of the first differing characters
- *         0 if strings are equal
- */
+* _strcmp - Compares two strings
+* @s1: First string
+* @s2: Second string
+*
+* Return: Difference between the ASCII values of the first differing characters
+*         0 if strings are equal
+*/
 int _strcmp(char *s1, char *s2)
 {
-    int i;
+int i;
 
-    for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
-    {
-        if (s1[i] != s2[i])
-            return (s1[i] - s2[i]);
-    }
-
-    return (0);
+for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+{
+if (s1[i] != s2[i])
+return (s1[i] - s2[i]);
 }
 
+return (0);
+}
