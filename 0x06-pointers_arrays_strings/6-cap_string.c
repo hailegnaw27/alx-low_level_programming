05891b10e6526e3742f3bf596bf3000b54a34f26
hailegnaw <ixvos406@gmail.com>
2023-06-29 13:10:57 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+/**
+* cap_string - Capitalizes all words of a string
+* @str: The string to capitalize
+*
+* Return: The capitalized string
+*/
+char *cap_string(char *str)
+{
+int i = 0;
+bool new_word = true;
+char separators[] = " \t\n,;.!?\"(){}";
+
+while (str[i] != '\0')
+{
+if (new_word && (str[i] >= 'a' && str[i] <= 'z'))
+{
+str[i] = str[i] - 32;
+}
+
+new_word = false;
+
+for (int j = 0; separators[j] != '\0'; j++)
+{
+if (str[i] == separators[j])
+{
+new_word = true;
+break;
+}
+}
+
+i++;
+}
+
+return (str);
+}
