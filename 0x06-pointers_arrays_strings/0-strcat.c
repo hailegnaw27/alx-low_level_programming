8944569cd3f43ad07dde9b2f90685d4245f29a4a
hailegnaw <ixvos406@gmail.com>
2023-06-29 12:35:22 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,32 +1,31 @@
 /**
- * _strcat - concatenates two strings
- * @dest: the destination string
- * @src: the source string
- *
- * Return: a pointer to the resulting string `dest`
- */
+* _strcat - concatenates two strings
+* @dest: the destination string
+* @src: the source string
+*
+* Return: a pointer to the resulting string `dest`
+*/
 char *_strcat(char *dest, char *src)
 {
-    int i = 0;
-    int j = 0;
+int i = 0;
+int j = 0;
 
-    /* Find the end of the destination string */
-    while (dest[i] != '\0')
-    {
-        i++;
-    }
-
-    /* Concatenate the source string to the destination string */
-    while (src[j] != '\0')
-    {
-        dest[i] = src[j];
-        i++;
-        j++;
-    }
-
-    /* Add the null terminator at the end */
-    dest[i] = '\0';
-
-    return dest;
+/* Find the end of the destination string */
+while (dest[i] != '\0')
+{
+i++;
 }
 
+/* Concatenate the source string to the destination string */
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+
+/* Add the null terminator at the end */
+dest[i] = '\0';
+
+return (dest);
+}
