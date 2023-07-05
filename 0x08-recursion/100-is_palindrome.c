42a83d74c574783e587e71d207e409f81ff16ed1
hailegnaw <ixvos406@gmail.com>
2023-07-05 15:10:20 -0700

    1000

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,40 +2,39 @@
 #include <stdio.h>
 
 /**
- * is_palindrome - checks if a string is a palindrome
- * @s: the string to check
- *
- * Return: 1 if the string is a palindrome, 0 otherwise
- */
+* is_palindrome - checks if a string is a palindrome
+* @s: the string to check
+*
+* Return: 1 if the string is a palindrome, 0 otherwise
+*/
 int is_palindrome(char *s)
 {
-    int len = 0;
+int len = 0;
 
-    if (*s == '\0')
-        return (1);
+if (*s == '\0')
+return (1);
 
-    while (s[len] != '\0')
-        len++;
+while (s[len] != '\0')
+len++;
 
-    return (check_palindrome(s, len - 1, 0));
+return (check_palindrome(s, len - 1, 0));
 }
 
 /**
- * check_palindrome - checks if a string is a palindrome recursively
- * @s: the string to check
- * @start: the starting index
- * @end: the ending index
- *
- * Return: 1 if the string is a palindrome, 0 otherwise
- */
+* check_palindrome - checks if a string is a palindrome recursively
+* @s: the string to check
+* @start: the starting index
+* @end: the ending index
+*
+* Return: 1 if the string is a palindrome, 0 otherwise
+*/
 int check_palindrome(char *s, int start, int end)
 {
-    if (start >= end)
-        return (1);
+if (start >= end)
+return (1);
 
-    if (s[start] != s[end])
-        return (0);
+if (s[start] != s[end])
+return (0);
 
-    return (check_palindrome(s, start + 1, end - 1));
+return (check_palindrome(s, start - 1, end + 1));
 }
-
