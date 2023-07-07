d8fda22e71ca789f4d761ca94fa88ba6d75cbc38
hailegnaw <ixvos406@gmail.com>
2023-07-07 13:37:33 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,52 +11,44 @@
 */
 int main(int argc, char *argv[])
 {
-int cents, coins = 0;
+	if (argc != 2)
+    {
+        printf("Error\n");
+        return 1;
+    }
 
-if (argc != 2)
-{
-printf("Error\n");
-return (1);
-}
+    int cents = atoi(argv[1]);
 
-cents = atoi(argv[1]);
+    if (cents < 0)
+    {
+        printf("0\n");
+        return 0;
+    }
 
-if (cents < 0)
-{
-printf("0\n");
-return (0);
-}
+    int coins = calculateCoins(cents);
+    printf("%d\n", coins);
 
-while (cents > 0)
-{
-if (cents >= 25)
-{
-cents -= 25;
-coins++;
-}
-else if (cents >= 10)
-{
-cents -= 10;
-coins++;
-}
-else if (cents >= 5)
-{
-cents -= 5;
-coins++;
-}
-else if (cents >= 2)
-{
-cents -= 2;
-coins++;
-}
-else
-{
-cents -= 1;
-coins++;
-}
+    return 0;
 }
 
-printf("%d\n", coins);
+int calculateCoins(int cents)
+{
+    int coins = 0;
+    while (cents > 0)
+    {
+        if (cents >= 25)
+            cents -= 25;
+        else if (cents >= 10)
+            cents -= 10;
+        else if (cents >= 5)
+            cents -= 5;
+        else if (cents >= 2)
+            cents -= 2;
+        else
+            cents -= 1;
 
-return (0);
+        coins++;
+    }
+
+    return (coins);
 }
