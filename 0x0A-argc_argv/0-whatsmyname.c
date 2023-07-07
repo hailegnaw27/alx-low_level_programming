06368376566db3b70e234a426d5a43fbd4b77db6
hailegnaw <ixvos406@gmail.com>
2023-07-07 11:42:30 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: The number of command line arguments
+ * @argv: An array containing the command line arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+/* Print the name of the program */
+printf("%s\n", argv[0]);
+
+return (0);
+}
+
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void print_name(char *name);
+void print_number_of_arguments(int argc);
+void print_arguments(char *argv[]);
+int multiply_numbers(int num1, int num2);
+int add_positive_numbers(int argc, char *argv[]);
+
+#endif /* MAIN_H */
+
