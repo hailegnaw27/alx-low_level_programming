22988ff8874464f245cc9aa6d6f6df0f205aaaec
hailegnaw <ixvos406@gmail.com>
2023-07-07 13:19:19 -0700

    main.h

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,13 +2,7 @@
 #define MAIN_H
 
 int _putchar(char c);
-void print_name(char *name);
-void print_number_of_arguments(int argc);
-void print_arguments(char *argv[]);
-int multiply_numbers(int num1, int num2);
-int add_positive_numbers(int argc, char *argv[]);
+int _atoi(char *s);
 int main(int argc, char *argv[]);
-void print_name(char *name);
-
 #endif /* MAIN_H */
 
