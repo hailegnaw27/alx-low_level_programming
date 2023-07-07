4e43d5f3e0f42fe9eb21638d68bda6a6df3981c9
hailegnaw <ixvos406@gmail.com>
2023-07-07 11:50:17 -0700

    main.h

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -7,6 +7,8 @@ void print_number_of_arguments(int argc);
 void print_arguments(char *argv[]);
 int multiply_numbers(int num1, int num2);
 int add_positive_numbers(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+void print_name(char *name);
 
 #endif /* MAIN_H */
 
