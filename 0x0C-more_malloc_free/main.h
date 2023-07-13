ea7a70610d4c3f4c619726cc45adc5b83a07e529
hailegnaw <ixvos406@gmail.com>
2023-07-13 12:33:35 -0700

    main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,6 +7,7 @@ void *_calloic(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
-
+int is_positive_number(const char* str);
+int main(int argc, char* argv[]);
 #endif /* MAIN_H */
 
