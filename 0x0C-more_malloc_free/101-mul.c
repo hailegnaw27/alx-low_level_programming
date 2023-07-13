df0b07e5c3d9d794880b69067f66f4d91a0d10ff
hailegnaw <ixvos406@gmail.com>
2023-07-13 12:30:47 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,54 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
-/**
- * main - Entry point
- * @argc: Number of command-line arguments
- * @argv: Array of command-line arguments
- *
- * Return: 0 on success, 98 on error
- */
-int main(int argc, char *argv[])
-{
-	/* Check the number of arguments */
-	if (argc != 3)
-	{
-		printf("Error\n");
-		return (98);
-	}
+#include <ctype.h>
 
-	/* Extract the numbers from command line arguments */
-	char *num1_str = argv[1];
-	char *num2_str = argv[2];
+int is_positive_number(const char* str) {
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (!isdigit(str[i])) {
+            return 0;
+        }
+    }
+    return 1;
+}
 
-	/* Check if the inputs are valid numbers */
-	for (int i = 0; num1_str[i] != '\0'; i++)
-	{
-		if (num1_str[i] < '0' || num1_str[i] > '9')
-		{
-			printf("Error\n");
-			return (98);
-		}
-	}
-	for (int i = 0; num2_str[i] != '\0'; i++)
-	{
-		if (num2_str[i] < '0' || num2_str[i] > '9')
-		{
-			printf("Error\n");
-			return (98);
-		}
-	}
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        printf("Error\n");
+        return (98);
+    }
 
-	/* Convert the numbers to integers */
-	int num1 = atoi(num1_str);
-	int num2 = atoi(num2_str);
+    if (!is_positive_number(argv[1]) || !is_positive_number(argv[2])) {
+        printf("Error\n");
+        return (98);
+    }
 
-	/* Multiply the numbers */
-	int result = num1 * num2;
+    int num1 = atoi(argv[1]);
+    int num2 = atoi(argv[2]);
+    int result = num1 * num2;
 
-	/* Print the result */
-	printf("%d\n", result);
+    printf("%d\n", result);
 
-	return (0);
+    return (0);
 }
 
