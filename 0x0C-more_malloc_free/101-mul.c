58e269895165c8fc523c2a86503d118cf81350a0
hailegnaw <ixvos406@gmail.com>
2023-07-13 12:48:04 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,33 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include "main.h"
-int is_positive_number(const char* str) {
-    for (int i = 0; str[i] != '\0'; i++) {
-        if (!isdigit(str[i])) {
-            return 0;
+
+int validate_arguments(int argc, char *argv[]) {
+    int i, j;
+
+    /* Check if the number of arguments is correct */
+    if (argc != 3) {
+        printf("Error\n");
+        return 0;
+    }
+
+    /* Check if both arguments are composed of digits */
+    for (i = 1; i < argc; i++) {
+        for (j = 0; argv[i][j] != '\0'; j++) {
+            if (!isdigit(argv[i][j])) {
+                printf("Error\n");
+                return 0;
+            }
         }
     }
+
     return 1;
 }
 
-int main(int argc, char* argv[]) {
-    if (argc != 3) {
-        printf("Error\n");
-        return (98);
-    }
+void multiply_numbers(char *num1, char *num2) {
+    /* Convert the string representations of num1 and num2 to integers */
+    int n1 = atoi(num1);
+    int n2 = atoi(num2);
 
-    if (!is_positive_number(argv[1]) || !is_positive_number(argv[2])) {
-        printf("Error\n");
-        return (98);
-    }
+    /* Multiply num1 and num2 */
+    int result = n1 * n2;
 
-    int num1 = atoi(argv[1]);
-    int num2 = atoi(argv[2]);
-    int result = num1 * num2;
-
+    /* Print the result */
     printf("%d\n", result);
+}
 
-    return (0);
+int main(int argc, char *argv[]) {
+    /* Validate the arguments */
+    if (!validate_arguments(argc, argv)) {
+        return 98;
+    }
+
+    /* Multiply the numbers */
+    multiply_numbers(argv[1], argv[2]);
+
+    return 0;
 }
 
