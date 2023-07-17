4d65fb365df129381b0a0593c73f3279f7b838f4
hailegnaw <ixvos406@gmail.com>
2023-07-17 10:25:09 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,9 +16,5 @@ struct dog
     char *owner;
 };
 
-/* Function prototypes */
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-
 #endif /* DOG_H */
 
