b34e54e62ffd0f29d0b616878d3823747b3a1510
hailegnaw <ixvos406@gmail.com>
2023-07-19 07:59:14 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,36 +2,35 @@
 #include <stdlib.h>
 
 /**
- * main - Entry point
- * @argc: Number of command-line arguments
- * @argv: Array of command-line arguments
- *
- * Return: 0 on success, 1 on incorrect number of arguments,
- *         2 on negative number of bytes
- */
+* main - Entry point
+* @argc: Number of command-line arguments
+* @argv: Array of command-line arguments
+*
+* Return: 0 on success, 1 on incorrect number of arguments,
+*         2 on negative number of bytes
+*/
 int main(int argc, char *argv[])
 {
-    int i, num_bytes;
+int i, num_bytes;
 
-    if (argc != 2)
-    {
-        printf("Error\n");
-        return (1);
-    }
-
-    num_bytes = atoi(argv[1]);
-
-    if (num_bytes < 0)
-    {
-        printf("Error\n");
-        return (2);
-    }
-
-    unsigned char *ptr = (unsigned char *)main;
-    for (i = 0; i < num_bytes; i++)
-        printf("%02x ", ptr[i]);
-    printf("\n");
-
-    return (0);
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
 }
 
+num_bytes = atoi(argv[1]);
+
+if (num_bytes < 0)
+{
+printf("Error\n");
+return (2);
+}
+
+unsigned char *ptr = (unsigned char *)main;
+for (i = 0; i < num_bytes; i++)
+printf("%02x ", ptr[i]);
+printf("\n");
+
+return (0);
+}
