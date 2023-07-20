c813f43226a0a3d376ab90ea250eb6f7c433c1a7
hailegnaw <ixvos406@gmail.com>
2023-07-20 12:05:11 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,90 +1,44 @@
 #include <stdio.h>
+#include <stdarg.h>
 #include "variadic_functions.h"
 
 /**
-* print_char - Prints a character.
-* @args: The va_list of arguments.
-*/
-void print_char(va_list args)
-{
-printf("%c", va_arg(args, int));
-}
-
-/**
-* print_int - Prints an integer.
-* @args: The va_list of arguments.
-*/
-void print_int(va_list args)
-{
-printf("%d", va_arg(args, int));
-}
-
-/**
-* print_float - Prints a float.
-* @args: The va_list of arguments.
-*/
-void print_float(va_list args)
-{
-printf("%f", va_arg(args, double));
-}
-
-/**
-* print_string - Prints a string.
-* @args: The va_list of arguments.
-*/
-void print_string(va_list args)
-{
-char *str = va_arg(args, char *);
-
-if (str != NULL)
-printf("%s", str);
-else
-printf("(nil)");
-}
-
-/**
-* print_all - Prints anything.
-* @format: A list of types of arguments passed to the function.
+* print_all - Prints anything based on the provided format
+* @format: A list of types of arguments passed to the function
 */
 void print_all(const char * const format, ...)
 {
 va_list args;
-unsigned int i = 0;
-char current_format;
-
+int i = 0;
+char *str;
 va_start(args, format);
-
 while (format && format[i])
 {
-current_format = format[i];
-
-switch (current_format)
+switch (format[i])
 {
 case 'c':
-print_char(args);
+printf("%c", va_arg(args, int));
 break;
 case 'i':
-print_int(args);
+printf("%d", va_arg(args, int));
 break;
 case 'f':
-print_float(args);
+printf("%f", va_arg(args, double));
 break;
 case 's':
-print_string(args);
+str = va_arg(args, char *);
+if (str == NULL)
+str = "(nil)";
+printf("%s", str);
 break;
 default:
 i++;
 continue;
 }
-
-if (format[i + 1] != '\0')
+if (format[i + 1])
 printf(", ");
-
 i++;
 }
-
-va_end(args);
-
 printf("\n");
+va_end(args);
 }
-
