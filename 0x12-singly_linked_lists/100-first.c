0f20167a00fbdda7be99cfe514fef50307a8ccfb
hailegnaw <ixvos406@gmail.com>
2023-07-27 12:38:29 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,25 +1,7 @@
 #include <stdio.h>
 
-void print_before_main(void) __attribute__((constructor));
-
-/**
- * print_before_main - Print the main function is executed
- */
-void print_before_main(void)
+void __attribute__((constructor)) before_main()
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
-}
-
-/**
- * main - Entry point
- *
- * Return: Always 0
- */
-int main(void)
-{
-	printf("(A tortoise, having pretty good sense of a hare's nature,\n");
-	printf(" challenges one to a race.)\n");
-	return (0);
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
 
