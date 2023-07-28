2b377d506f6226d2ecef0edd9f8ddd5ea4e003f5
hailegnaw <ixvos406@gmail.com>
2023-07-27 17:44:34 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,19 @@
 section .data
-    hello db "Hello, Holberton", 0
+    hello db 'Hello, Holberton', 0
 
 section .text
     global _start
 
 _start:
     ; Write the string to stdout
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, hello
-    mov edx, 16
-    int 0x80
+    mov rax, 1                  ; System call number for write
+    mov rdi, 1                  ; File descriptor for stdout
+    mov rsi, hello              ; Address of the string
+    mov rdx, 17                 ; Length of the string
+    syscall
 
     ; Exit the program
-    mov eax, 1
-    xor ebx, ebx
-    int 0x80
+    mov eax, 60                 ; System call number for exit
+    xor edi, edi                ; Exit status 0
+    syscall
 
