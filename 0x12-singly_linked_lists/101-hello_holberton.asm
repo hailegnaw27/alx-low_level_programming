bf3429bfe1e21ed54af2994fda1675c69c8fe40e
hailegnaw <ixvos406@gmail.com>
2023-07-27 18:03:55 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -8,13 +8,13 @@ section .text
 _start_asm:
     ; Load the address of the hello string into a register
     mov rdi, hello
-    
+
     ; Call the printf function
     mov rax, 0x2000004 ; System call number for write
     mov rsi, rdi      ; Address of the string
     mov rdx, 16       ; Length of the string
     syscall
-    
+
     ; Set the exit system call number (60) into a register
     mov rax, 0x2000001 ; System call number for exit
     xor rdi, rdi      ; Exit status (0)
