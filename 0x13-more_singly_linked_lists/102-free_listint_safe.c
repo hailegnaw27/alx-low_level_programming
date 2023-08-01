30dbbbf8aa5335f5bef5c287343e4b9430594bf9
hailegnaw <ixvos406@gmail.com>
2023-08-01 15:16:30 -0700

    102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,59 +1,31 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
 
 /**
-* print_listint_safe - Prints a listint_t linked list.
-* @head: Pointer to the head of the list.
-*
-* Return: The number of nodes in the list.
-*/
-size_t print_listint_safe(const listint_t *head)
-{
-size_t count = 0;
-const listint_t *current = head;
-const listint_t *next = NULL;
-
-while (current != NULL)
-{
-count++;
-printf("[%p] %d\n", (void *)current, current->n);
-next = current->next;
-
-if (next >= current)
-{
-printf("-> [%p] %d\n", (void *)next, next->n);
-break;
-}
-
-current = next;
-}
-
-return (count);
-}
-
-/**
-* free_listint_safe - Frees a listint_t linked list.
-* @head: Double pointer to the head of the list.
-*
-* Return: The size of the list that was freed.
+* free_listint_safe - Frees a listint_t linked list (safe version)
+* @head: Double pointer to the head of the list
+* Return: Number of nodes that were freed
 */
 size_t free_listint_safe(listint_t **head)
 {
 size_t count = 0;
 listint_t *current = *head;
-listint_t *next = NULL;
+listint_t *next_node = NULL;
 
 while (current != NULL)
 {
 count++;
-next = current->next;
+
+next_node = current->next;
 free(current);
 
-if (next >= current)
+/* Check if next_node points back to a previous node */
+if (next_node >= current)
+{
 break;
+}
 
-current = next;
+current = next_node;
 }
 
 *head = NULL;
