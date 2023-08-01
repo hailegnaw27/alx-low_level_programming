cd6b33d42782db91c5a296edc4fbf22afb2d3dbb
hailegnaw <ixvos406@gmail.com>
2023-08-01 15:02:14 -0700

    102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,64 +1,62 @@
-#include "lists.h"
-#include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
 
 /**
-* free_listint_safe - Frees a listint_t list.
-* @h: Pointer to the head node of the list.
+* print_listint_safe - Prints a listint_t linked list.
+* @head: Pointer to the head of the list.
+*
+* Return: The number of nodes in the list.
+*/
+size_t print_listint_safe(const listint_t *head)
+{
+size_t count = 0;
+const listint_t *current = head;
+const listint_t *next = NULL;
+
+while (current != NULL)
+{
+count++;
+printf("[%p] %d\n", (void *)current, current->n);
+next = current->next;
+
+if (next >= current)
+{
+printf("-> [%p] %d\n", (void *)next, next->n);
+break;
+}
+
+current = next;
+}
+
+return (count);
+}
+
+/**
+* free_listint_safe - Frees a listint_t linked list.
+* @head: Double pointer to the head of the list.
 *
 * Return: The size of the list that was freed.
 */
-size_t free_listint_safe(listint_t **h)
+size_t free_listint_safe(listint_t **head)
 {
-listint_t *slow = *h, *fast = *h;
 size_t count = 0;
+listint_t *current = *head;
+listint_t *next = NULL;
 
-if (h == NULL || *h == NULL)
-return (0);
-
-while (fast && fast->next)
+while (current != NULL)
 {
-slow = slow->next;
-fast = fast->next->next;
-
-if (slow == fast)
-{
-slow = *h;
-while (slow != fast)
-{
-slow = slow->next;
-fast = fast->next;
-}
-fast = fast->next; // Move fast one step ahead to get the last node of the loop
-while (fast->next != slow)
-{
-fast = fast->next;
 count++;
-}
-fast->next = NULL; // Break the loop
+next = current->next;
+free(current);
 
-slow = *h;
-while (slow)
-{
-fast = slow->next;
-free(slow);
-count++;
-slow = fast;
-}
-*h = NULL;
-return (count);
-}
+if (next >= current)
+break;
+
+current = next;
 }
 
-slow = *h;
-while (slow)
-{
-fast = slow->next;
-free(slow);
-count++;
-slow = fast;
-}
-*h = NULL;
+*head = NULL;
+
 return (count);
 }
