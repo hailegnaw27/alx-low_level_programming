47a95f9e78711fcaf8941bd79cfad66a17038451
hailegnaw <ixvos406@gmail.com>
2023-08-01 14:30:20 -0700

    102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,48 +1,61 @@
-#include <stdlib.h>
 #include "lists.h"
 
 /**
-* free_listint_safe - Frees a listint_t linked list.
-* @h: Pointer to a pointer to the head node of the list.
+* free_listint_safe - Frees a listint_t list.
+* @h: Pointer to the head node of the list.
 *
 * Return: The size of the list that was freed.
 */
 size_t free_listint_safe(listint_t **h)
 {
-listint_t *slow = *h;
-listint_t *fast = *h;
+listint_t *slow = *h, *fast = *h;
 size_t count = 0;
 
-while (fast != NULL && fast->next != NULL)
+if (h == NULL || *h == NULL)
+return (0);
+
+while (fast && fast->next)
 {
 slow = slow->next;
 fast = fast->next->next;
-count++;
 
 if (slow == fast)
 {
-*h = NULL;
-while (*h != slow)
-{
-*h = slow->next;
-free(slow);
 slow = *h;
+while (slow != fast)
+{
+slow = slow->next;
+fast = fast->next;
+}
+fast = fast->next; // Move fast one step ahead to get the last node of the loop
+while (fast->next != slow)
+{
+fast = fast->next;
 count++;
 }
+fast->next = NULL; // Break the loop
+
+slow = *h;
+while (slow)
+{
+fast = slow->next;
 free(slow);
 count++;
+slow = fast;
+}
+*h = NULL;
 return (count);
 }
 }
 
-*h = NULL;
-while (slow != NULL)
-{
-*h = slow->next;
-free(slow);
 slow = *h;
+while (slow)
+{
+fast = slow->next;
+free(slow);
 count++;
+slow = fast;
 }
-
+*h = NULL;
 return (count);
 }
