e17d729697d54f758c476426e9c14b8bdde2c49d
hailegnaw <ixvos406@gmail.com>
2023-08-01 15:25:07 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,34 +1,34 @@
+#include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "lists.h"
 
 /**
-* print_listint_safe - Prints a listint_t linked list (safe version)
-* @head: Pointer to the head of the list
-* Return: Number of nodes in the list
-*/
+ * print_listint_safe - Prints a listint_t linked list
+ * @head: Pointer to the head of the list
+ *
+ * Return: The number of nodes in the list
+ */
 size_t print_listint_safe(const listint_t *head)
 {
-size_t count = 0;
-const listint_t *current = head;
-const listint_t *next_node = NULL;
+	size_t count = 0;
+	const listint_t *current = head;
+	const listint_t *temp = NULL;
 
-while (current != NULL)
-{
-count++;
-printf("[%p] %d\n", (void *)current, current->n);
+	while (current != NULL)
+	{
+		printf("[%p] %d\n", (void *)current, current->n);
+		count++;
+		temp = current;
+		current = current->next;
 
-next_node = current->next;
+		/* Check for loop */
+		if (temp <= current)
+		{
+			printf("-> [%p] %d\n", (void *)current, current->n);
+			break;
+		}
+	}
 
-/* Check if next_node points back to a previous node */
-if (next_node >= current)
-{
-printf("-> [%p] %d\n", (void *)next_node, next_node->n);
-break;
+	return (count);
 }
 
-current = current->next;
-}
-
-return (count);
-}
