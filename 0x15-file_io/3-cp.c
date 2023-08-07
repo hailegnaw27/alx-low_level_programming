51eb81721aebbce1a8027bdade9702cef9b01ee6
hailegnaw <ixvos406@gmail.com>
2023-08-07 10:38:54 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,61 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include "main.h"
 
-#define BUFFER_SIZE 1024
-
-/* Function to handle error and exit */
-void handle_error(const char *error_message)
+/**
+ * error_exit - Prints an error message and exits the program.
+ * @message: The error message to print.
+ * @code: The exit code.
+ */
+void error_exit(const char *message, int code)
 {
-    perror(error_message);
-    exit(EXIT_FAILURE);
+    dprintf(STDERR_FILENO, "%s\n", message);
+    exit(code);
 }
 
-/* Function to copy file_from to file_to */
-void copy_file(const char *file_from, const char *file_to)
-{
-    int fd_from, fd_to, read_bytes, write_bytes;
-    char buffer[BUFFER_SIZE];
-
-    /* Open file_from for reading */
-    fd_from = open(file_from, O_RDONLY);
-    if (fd_from == -1)
-        handle_error("Error: Can't read from file");
-
-    /* Open file_to for writing */
-    fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    if (fd_to == -1)
-        handle_error("Error: Can't write to file");
-
-    /* Copy content from file_from to file_to */
-    while ((read_bytes = read(fd_from, buffer, BUFFER_SIZE)) > 0) {
-        write_bytes = write(fd_to, buffer, read_bytes);
-        if (write_bytes != read_bytes)
-            handle_error("Error: Can't write to file");
-    }
-
-    /* Check for read error */
-    if (read_bytes == -1)
-        handle_error("Error: Can't read from file");
-
-    /* Close file descriptors */
-    if (close(fd_from) == -1 || close(fd_to) == -1)
-        handle_error("Error: Can't close file descriptor");
-
-    printf("File copied successfully.\n");
-}
-
+/**
+ * main - Copies the content of a file to another file.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of command-line argument strings.
+ * Return: 0 on success, or the appropriate exit code on failure.
+ */
 int main(int argc, char *argv[])
 {
-    /* Check for correct number of arguments */
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s file_from file_to\n", argv[0]);
-        exit(EXIT_FAILURE);
+    int file_from, file_to, read_count, write_count;
+    char buffer[1024];
+
+    if (argc != 3)
+        error_exit("Usage: cp file_from file_to", 97);
+
+    file_from = open(argv[1], O_RDONLY);
+    if (file_from == -1)
+        error_exit("Error: Can't read from file", 98);
+
+    file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (file_to == -1)
+        error_exit("Error: Can't write to file", 99);
+
+    while ((read_count = read(file_from, buffer, sizeof(buffer))) > 0)
+    {
+        write_count = write(file_to, buffer, read_count);
+        if (write_count == -1)
+            error_exit("Error: Can't write to file", 99);
+        if (write_count < read_count)
+            error_exit("Error: Can't write to file", 99);
     }
 
-    /* Copy file */
-    copy_file(argv[1], argv[2]);
+    if (read_count == -1)
+        error_exit("Error: Can't read from file", 98);
+
+    if (close(file_from) == -1)
+        error_exit("Error: Can't close file descriptor", 100);
+
+    if (close(file_to) == -1)
+        error_exit("Error: Can't close file descriptor", 100);
 
     return 0;
 }
