3dffa931d7847af9ce18679e1560054cdffd1358
hailegnaw <ixvos406@gmail.com>
2023-08-07 10:27:40 -0700

    3-cp.c

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 1024
+
+/**
+ * error_exit - Print error message and exit with given exit code
+ * @message: The error message to print
+ * @exit_code: The exit code to exit with
+ */
+void error_exit(char *message, int exit_code)
+{
+	dprintf(STDERR_FILENO, "%s\n", message);
+	exit(exit_code);
+}
+
+/**
+ * main - Entry point
+ * @argc: The number of command-line arguments
+ * @argv: The command-line arguments
+ *
+ * Return: 0 on success, otherwise the exit code
+ */
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to, bytes_read, bytes_written;
+	char buffer[BUFFER_SIZE];
+
+	if (argc != 3)
+		error_exit("Usage: cp file_from file_to", 97);
+
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+		error_exit("Error: Can't read from file", 98);
+
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (fd_to == -1)
+		error_exit("Error: Can't write to file", 99);
+
+	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written != bytes_read || bytes_written == -1)
+			error_exit("Error: Can't write to file", 99);
+	}
+
+	if (bytes_read == -1)
+		error_exit("Error: Can't read from file", 98);
+
+	if (close(fd_from) == -1)
+		error_exit("Error: Can't close fd", 100);
+
+	if (close(fd_to) == -1)
+		error_exit("Error: Can't close fd", 100);
+
+	return (0);
+}
+
