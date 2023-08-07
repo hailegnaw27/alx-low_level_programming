385c4f6af5bdce5281c4592acb4e97e070f718ec
hailegnaw <ixvos406@gmail.com>
2023-08-07 10:34:57 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,56 +5,58 @@
 
 #define BUFFER_SIZE 1024
 
-/**
- * error_exit - Print error message and exit with given exit code
- * @message: The error message to print
- * @exit_code: The exit code to exit with
- */
-void error_exit(char *message, int exit_code)
+/* Function to handle error and exit */
+void handle_error(const char *error_message)
 {
-	dprintf(STDERR_FILENO, "%s\n", message);
-	exit(exit_code);
+    perror(error_message);
+    exit(EXIT_FAILURE);
 }
 
-/**
- * main - Entry point
- * @argc: The number of command-line arguments
- * @argv: The command-line arguments
- *
- * Return: 0 on success, otherwise the exit code
- */
+/* Function to copy file_from to file_to */
+void copy_file(const char *file_from, const char *file_to)
+{
+    int fd_from, fd_to, read_bytes, write_bytes;
+    char buffer[BUFFER_SIZE];
+
+    /* Open file_from for reading */
+    fd_from = open(file_from, O_RDONLY);
+    if (fd_from == -1)
+        handle_error("Error: Can't read from file");
+
+    /* Open file_to for writing */
+    fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (fd_to == -1)
+        handle_error("Error: Can't write to file");
+
+    /* Copy content from file_from to file_to */
+    while ((read_bytes = read(fd_from, buffer, BUFFER_SIZE)) > 0) {
+        write_bytes = write(fd_to, buffer, read_bytes);
+        if (write_bytes != read_bytes)
+            handle_error("Error: Can't write to file");
+    }
+
+    /* Check for read error */
+    if (read_bytes == -1)
+        handle_error("Error: Can't read from file");
+
+    /* Close file descriptors */
+    if (close(fd_from) == -1 || close(fd_to) == -1)
+        handle_error("Error: Can't close file descriptor");
+
+    printf("File copied successfully.\n");
+}
+
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to, bytes_read, bytes_written;
-	char buffer[BUFFER_SIZE];
+    /* Check for correct number of arguments */
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s file_from file_to\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
 
-	if (argc != 3)
-		error_exit("Usage: cp file_from file_to", 97);
+    /* Copy file */
+    copy_file(argv[1], argv[2]);
 
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-		error_exit("Error: Can't read from file", 98);
-
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (fd_to == -1)
-		error_exit("Error: Can't write to file", 99);
-
-	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
-	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written == -1 || bytes_written != bytes_read)
-			error_exit("Error: Can't write to file", 99);
-	}
-
-	if (bytes_read == -1)
-		error_exit("Error: Can't read from file", 98);
-
-	if (close(fd_from) == -1)
-		error_exit("Error: Can't close fd", 100);
-
-	if (close(fd_to) == -1)
-		error_exit("Error: Can't close fd", 100);
-
-	return (0);
+    return 0;
 }
 
