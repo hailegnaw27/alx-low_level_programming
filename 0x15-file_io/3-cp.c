23587d140010aed4a811181321e47141daa18e36
hailegnaw27 <ixvos406@gmail.com>
2023-09-04 13:53:30 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,14 +2,15 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include "main.h"
 
-#define BUF_SIZE 1024
+#define BUFFER_SIZE 1024
 
 /**
  * print_error - Prints the error message and exits with the given code
  * @code: The exit code
  * @msg: The error message
+ *
+ * Return: None
  */
 void print_error(int code, const char *msg)
 {
@@ -18,35 +19,62 @@ void print_error(int code, const char *msg)
 }
 
 /**
- * main - Entry point
- * @argc: Number of arguments
- * @argv: Array of arguments
- * Return: 0 on success, otherwise a positive integer
+ * open_file - Opens a file with the given flags
+ * @filename: The name of the file to open
+ * @flags: The flags to use when opening the file
+ *
+ * Return: The file descriptor of the opened file
  */
-int main(int argc, char *argv[])
+int open_file(const char *filename, int flags)
 {
-	int fd_from, fd_to, rd, wr;
-	char buffer[BUF_SIZE];
-
-	if (argc != 3)
-		print_error(97, "Usage: cp file_from file_to");
-
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
+	int fd = open(filename, flags);
+	if (fd == -1)
 		print_error(98, "Error: Can't read from file");
 
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	return (fd);
+}
+
+/**
+ * write_file - Writes data to a file
+ * @fd: The file descriptor of the file to write to
+ * @filename: The name of the file being written to (for error messages)
+ * @buffer: The buffer containing the data to write
+ * @size: The size of the data to write
+ *
+ * Return: None
+ */
+void write_file(int fd, const char *filename, const char *buffer, ssize_t size)
+{
+	ssize_t written = write(fd, buffer, size);
+	if (written == -1 || written != size)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+		exit(99);
+	}
+}
+
+/**
+ * copy_file - Copies the content of one file to another file
+ * @file_from: The name of the file to copy from
+ * @file_to: The name of the file to copy to
+ *
+ * Return: None
+ */
+void copy_file(const char *file_from, const char *file_to)
+{
+	int fd_from, fd_to;
+	char buffer[BUFFER_SIZE];
+	ssize_t read_chars;
+
+	fd_from = open_file(file_from, O_RDONLY);
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 		print_error(99, "Error: Can't write to file");
 
-	while ((rd = read(fd_from, buffer, BUF_SIZE)) > 0)
-	{
-		wr = write(fd_to, buffer, rd);
-		if (wr == -1 || wr != rd)
-			print_error(99, "Error: Can't write to file");
-	}
+	while ((read_chars = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+		write_file(fd_to, file_to, buffer, read_chars);
 
-	if (rd == -1)
+	if (read_chars == -1)
 		print_error(98, "Error: Can't read from file");
 
 	if (close(fd_from) == -1)
@@ -54,6 +82,24 @@ int main(int argc, char *argv[])
 
 	if (close(fd_to) == -1)
 		print_error(100, "Error: Can't close file descriptor");
+}
+
+/**
+ * main - Entry point
+ * @argc: Number of arguments
+ * @argv: Array of arguments
+ *
+ * Return: 0 on success, 97 on incorrect number of arguments
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	copy_file(argv[1], argv[2]);
 
 	return (0);
 }
