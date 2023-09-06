379896a82f5f700b00912f4cfda0d8dba494b054
hailegnaw27 <ixvos406@gmail.com>
2023-09-06 12:19:22 -0700

    100-elf_header.c

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <elf.h>
+
+#define BUFFER_SIZE 128
+
+/**
+ * print_error - Prints an error message to stderr and exits with status code 98
+ * @msg: The error message to print
+ */
+void print_error(char *msg)
+{
+	fprintf(stderr, "%s\n", msg);
+	exit(98);
+}
+
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments
+ * @argv: An array of command-line argument strings
+ *
+ * Return: 0 on success, 98 on failure
+ */
+int main(int argc, char *argv[])
+{
+	int fd;
+	ssize_t bytes_read;
+	Elf64_Ehdr header;
+	char buffer[BUFFER_SIZE];
+
+	if (argc != 2)
+	{
+		print_error("Usage: elf_header elf_filename");
+	}
+
+	/* Open the ELF file */
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		print_error("Error opening file");
+	}
+
+	/* Read the ELF header */
+	bytes_read = read(fd, &header, sizeof(header));
+	if (bytes_read != sizeof(header))
+	{
+		print_error("Error reading ELF header");
+	}
+
+	/* Check if it's a valid ELF file */
+	if (header.e_ident[EI_MAG0] != ELFMAG0 ||
+	    header.e_ident[EI_MAG1] != ELFMAG1 ||
+	    header.e_ident[EI_MAG2] != ELFMAG2 ||
+	    header.e_ident[EI_MAG3] != ELFMAG3)
+	{
+		print_error("Not an ELF file");
+	}
+
+	/* Print the ELF header information */
+	printf("ELF Header:\n");
+	printf("  Magic:   ");
+	for (int i = 0; i < EI_NIDENT; i++)
+		printf("%02x ", header.e_ident[i]);
+	printf("\n");
+	printf("  Class:                             ");
+	/* Print the class information... */
+	/* Add code to print the rest of the header information... */
+
+	close(fd);
+	return 0;
+}
+
