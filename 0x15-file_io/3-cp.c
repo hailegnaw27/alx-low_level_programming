4dd72adcf29f16fe849ebcc2bc0f933e80bfcc88
hailegnaw27 <ixvos406@gmail.com>
2023-09-08 08:06:55 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,63 +1,89 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include "main.h"
 
 #define BUFFER_SIZE 1024
 
+/**
+ * print_usage_error - Prints the usage error message and exits.
+ */
+void print_usage_error(void)
+{
+    dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+    exit(97);
+}
+
+/**
+ * print_read_error - Prints the read error message and exits.
+ * @filename: The name of the file that couldn't be read.
+ */
+void print_read_error(char *filename)
+{
+    dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+    exit(98);
+}
+
+/**
+ * print_write_error - Prints the write error message and exits.
+ * @filename: The name of the file that couldn't be written to.
+ */
+void print_write_error(char *filename)
+{
+    dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+    exit(99);
+}
+
+/**
+ * print_close_error - Prints the close error message and exits.
+ * @fd: The file descriptor that couldn't be closed.
+ */
+void print_close_error(int fd)
+{
+    dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+    exit(100);
+}
+
+/**
+ * main - Entry point
+ * @argc: The number of command-line arguments
+ * @argv: An array of command-line argument strings
+ *
+ * Return: 0 on success, otherwise exit with an error code
+ */
 int main(int argc, char *argv[])
 {
-    int fd_from, fd_to, read_chars, write_chars;
+    int fd_from, fd_to, read_bytes, write_bytes;
     char buffer[BUFFER_SIZE];
 
     if (argc != 3)
-    {
-        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-        exit(97);
-    }
+        print_usage_error();
 
     fd_from = open(argv[1], O_RDONLY);
     if (fd_from == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-        exit(98);
-    }
+        print_read_error(argv[1]);
 
     fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
     if (fd_to == -1)
+        print_write_error(argv[2]);
+
+    while ((read_bytes = read(fd_from, buffer, BUFFER_SIZE)) > 0)
     {
-        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-        exit(99);
+        write_bytes = write(fd_to, buffer, read_bytes);
+        if (write_bytes == -1 || write_bytes != read_bytes)
+            print_write_error(argv[2]);
     }
 
-    while ((read_chars = read(fd_from, buffer, BUFFER_SIZE)) > 0)
-    {
-        write_chars = write(fd_to, buffer, read_chars);
-        if (write_chars != read_chars)
-        {
-            dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-            exit(99);
-        }
-    }
-
-    if (read_chars == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-        exit(98);
-    }
+    if (read_bytes == -1)
+        print_read_error(argv[1]);
 
     if (close(fd_from) == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-        exit(100);
-    }
+        print_close_error(fd_from);
 
     if (close(fd_to) == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-        exit(100);
-    }
+        print_close_error(fd_to);
 
-    return (0);
+    return 0;
 }
 
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -16,6 +16,11 @@ int append_text_to_file(const char *filename, char *text_content);
 int cp_file(const char *file_from, const char *file_to);
 void print_error(int code, const char *msg);
 void error_exit(char *msg, char *file, int fd, int code);
+void print_usage_error(void);
+void print_read_error(char *filename);
+void print_write_error(char *filename);
+void print_close_error(int fd);
+int main(int argc, char *argv[]);
 /* Helper function */
 int _putchar(char c);
 
