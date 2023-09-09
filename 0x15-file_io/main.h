58622f40b9ceb26cefb85614241495622616708e
hailegnaw27 <ixvos406@gmail.com>
2023-09-09 01:30:54 -0700

    main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -23,18 +23,5 @@ void print_close_error(int fd);
 int main(int argc, char *argv[]);
 /* Helper function */
 int _putchar(char c);
-/* file 100-elf_header.c */
 
-void print_elf_header(Elf64_Ehdr *header);
-int elf_header(const char *filename);
-void check_elf(unsigned char *e_ident);
-void print_magic(unsigned char *e_ident);
-void print_class(unsigned char *e_ident);
-void print_data(unsigned char *e_ident);
-void print_version(unsigned char *e_ident);
-void print_abi(unsigned char *e_ident);
-void print_osabi(unsigned char *e_ident);
-void print_type(unsigned int e_type, unsigned char *e_ident);
-void print_entry(unsigned long int e_entry, unsigned char *e_ident);
-void close_elf(int elf);
 #endif /* MAIN_H */
