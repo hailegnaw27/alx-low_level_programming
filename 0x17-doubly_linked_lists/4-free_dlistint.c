bc4d2b29c69f95af4fd5c2a55a559d19e1475d7d
hailegnaw27 <ixvos406@gmail.com>
2023-10-12 14:31:48 -0700

    doubly linked list

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -2,17 +2,17 @@
 #include <stdlib.h>
 
 /**
- * free_dlistint - Frees a dlistint_t list
- * @head: Pointer to the head of the list
- */
+* free_dlistint - Frees a dlistint_t list
+* @head: Pointer to the head of the list
+*/
 void free_dlistint(dlistint_t *head)
 {
-    dlistint_t *temp;
+dlistint_t *temp;
 
-    while (head != NULL)
-    {
-        temp = head->next;
-        free(head);
-        head = temp;
-    }
+while (head != NULL)
+{
+temp = head->next;
+free(head);
+head = temp;
+}
 }
--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,14 +1,19 @@
 #include "lists.h"
-
+/**
+* sum_dlistint - Computes the sum of all the data (n) of a doubl  list
+* @head: Pointer to the head of the list
+*
+* Return: The sum of the data (n) of the list, or 0 if the list is empty
+*/
 int sum_dlistint(dlistint_t *head)
 {
-    int sum = 0;
+int sum = 0;
 
-    while (head != NULL)
-    {
-        sum += head->n;
-        head = head->next;
-    }
+while (head != NULL)
+{
+sum += head->n;
+head = head->next;
+}
 
-    return (sum);
+return (sum);
 }
