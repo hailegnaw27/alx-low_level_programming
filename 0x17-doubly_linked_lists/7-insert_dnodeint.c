7cefd639f58055d22ce22060884bd12ea0ca5ef8
hailegnaw27 <ixvos406@gmail.com>
2023-10-12 14:02:43 -0700

    doubly_linked_lists

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,55 @@
+#include "lists.h"
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+    dlistint_t *new_node, *current;
+    unsigned int i;
+
+    /* Allocate memory for the new node */
+    new_node = malloc(sizeof(dlistint_t));
+    if (new_node == NULL)
+        return NULL;
+
+    /* Set the data of the new node */
+    new_node->n = n;
+    new_node->prev = NULL;
+    new_node->next = NULL;
+
+    /* If the list is empty, insert the new node as the head */
+    if (*h == NULL)
+    {
+        *h = new_node;
+        return new_node;
+    }
+
+    /* If the new node should be inserted at the beginning */
+    if (idx == 0)
+    {
+        new_node->next = *h;
+        (*h)->prev = new_node;
+        *h = new_node;
+        return new_node;
+    }
+
+    /* Traverse the list to find the node at index idx-1 */
+    current = *h;
+    for (i = 0; i < idx - 1 && current != NULL; i++)
+        current = current->next;
+
+    /* If the given index is out of range */
+    if (current == NULL)
+    {
+        free(new_node);
+        return NULL;
+    }
+
+    /* Update the pointers to insert the new node */
+    new_node->next = current->next;
+    new_node->prev = current;
+    if (current->next != NULL)
+        current->next->prev = new_node;
+    current->next = new_node;
+
+    return new_node;
+}
+
---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - Deletes the node at a given index
+ * @head: Pointer to the head of the doubly linked list
+ * @index: Index of the node to delete
+ *
+ * Return: 1 if successful, -1 if failed
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *current = *head;
+
+	if (*head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(current);
+		return (1);
+	}
+
+	for (unsigned int i = 0; i < index; i++)
+	{
+		if (current == NULL)
+			return (-1);
+		current = current->next;
+	}
+
+	if (current == NULL)
+		return (-1);
+
+	if (current->prev != NULL)
+		current->prev->next = current->next;
+
+	if (current->next != NULL)
+		current->next->prev = current->prev;
+
+	free(current);
+	return (1);
+}
+
