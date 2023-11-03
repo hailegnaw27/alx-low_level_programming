817d1f2d6802dd5bb3dffaa5aa2e39c357ede6eb
hailegnaw27 <ixvos406@gmail.com>
2023-11-03 04:00:15 -0700

    6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -2,35 +2,35 @@
 #include <stdlib.h>
 
 /**
- * hash_table_delete - Deletes a hash table.
- * @ht: The hash table to delete.
- */
+* hash_table_delete - Deletes a hash table.
+* @ht: The hash table to delete.
+*/
 void hash_table_delete(hash_table_t *ht)
 {
-    unsigned long int i;
-    hash_node_t *node, *temp;
+unsigned long int i;
+hash_node_t *node, *temp;
 
-    if (ht == NULL)
-        return;
+if (ht == NULL)
+return;
 
-    /* Free the memory for each node in the linked lists */
-    for (i = 0; i < ht->size; i++)
-    {
-        node = ht->array[i];
-        while (node != NULL)
-        {
-            temp = node;
-            node = node->next;
-            free(temp->key);
-            free(temp->value);
-            free(temp);
-        }
-    }
+/* Free the memory for each node in the linked lists */
+for (i = 0; i < ht->size; i++)
+{
+node = ht->array[i];
+while (node != NULL)
+{
+temp = node;
+node = node->next;
+free(temp->key);
+free(temp->value);
+free(temp)
+}
+}
 
-    /* Free the memory for the array of pointers */
-    free(ht->array);
+/* Free the memory for the array of pointers */
+free(ht->array);
 
-    /* Free the memory for the hash table structure */
-    free(ht);
+/* Free the memory for the hash table structure */
+free(ht);
 }
 
